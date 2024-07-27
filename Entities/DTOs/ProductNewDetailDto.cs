a2ea35a4c5b44ff13bf8897305f99f732452a0d0
Queen A <139057414+kildaci@users.noreply.github.com>
2024-07-27 23:48:19 +0300

    Create ProductNewDetailDto.cs

---
+++ Entities/DTOs/ProductNewDetailDto.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTOs
+{
+    public class ProductNewDetailDto:IDto
+    {
+        public int Id { get; set; }
+
+        public string CategoryName { get; set; }
+        public string CategoryNameSecond { get; set; }
+
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
