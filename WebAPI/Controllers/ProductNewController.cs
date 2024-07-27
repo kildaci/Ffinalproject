c786c1b572bda518d9cbf9e90d52c9d1681fb1ce
Queen A <139057414+kildaci@users.noreply.github.com>
2024-07-27 23:48:47 +0300

    Create ProductNewController.cs

---
+++ WebAPI/Controllers/ProductNewController.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductNewController : ControllerBase
+    {
+        IProductNewService _productNewService;
+        public ProductNewController(IProductNewService productNewService)
+        {
+            _productNewService = productNewService;
+        }
+        [HttpGet("getall")]
+        public IActionResult Get()
+        {
+            var result = _productNewService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+    }
+}
