5a7f07701efbfc2788220a41f8d5ddc2e568abf0
Queen A <139057414+kildaci@users.noreply.github.com>
2024-07-27 23:42:46 +0300

    Create IUserNewService.cs

---
+++ Business/Abstract/IUserNewService.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities.Concrete;
+using Core.Utilities.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IUserNewService
+    {
+        IDataResult<List<UserNew>> GetAll();
+        void Add(UserNew userNew);
+        UserNew GetByMail(string email);
+        List<OperationClaim> GetClaims(UserNew userNew);
+    }
+}
