666e8edf96f4872e73ef0b163ea2998848e49f33
Queen A <139057414+kildaci@users.noreply.github.com>
2024-07-27 23:47:39 +0300

    Create EfUserNewDal.cs

---
+++ DataAccess/Concrete/EntityFramework/EfUserNewDal.cs
@@ -0,0 +1,28 @@
+﻿using Core.DataAccess.EntityFramework;
+using Core.Entities.Concrete;
+using DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfUserNewDal : EfEntityRepositoryBase<UserNew, NorthwindContext>, IUserNewDal
+    {
+        public List<OperationClaim> GetClaims(UserNew userNew)
+        {
+            using (var context = new NorthwindContext())
+            {
+                var result = from operationClaim in context.OperationClaims
+                             join userOperationClaim in context.UserOperationClaims
+                                 on operationClaim.Id equals userOperationClaim.OperationClaimId
+                             where userOperationClaim.UserId == userNew.Id
+                             select new OperationClaim { Id = operationClaim.Id, Name = operationClaim.Name };
+                return result.ToList();
+
+            }
+        }
+    }
+}
