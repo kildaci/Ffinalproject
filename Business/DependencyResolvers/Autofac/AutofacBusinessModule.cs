9051ac991d96551f839b8be86d90ed7eec74c884
Queen A <139057414+kildaci@users.noreply.github.com>
2024-07-27 23:43:41 +0300

    Create AutofacBusinessModule.cs

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,51 @@
+﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Business.Abstract;
+using Business.CCS;
+using Business.Concrete;
+using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
+using Core.Utilities.Security.JWT;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule :Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+
+            builder.RegisterType<ProductNewManager>().As<IProductNewService>().SingleInstance();
+            builder.RegisterType<EfProductNewDal>().As<IProductNewDal>().SingleInstance();
+
+
+            builder.RegisterType<CategoryManager>().As<ICategoryService>().SingleInstance();
+            builder.RegisterType<EfCategoryDal>().As<ICategoryDal>().SingleInstance();
+            builder.RegisterType<UserManager>().As<IUserService>();
+            builder.RegisterType<UserNewManager>().As<IUserNewService>();
+            builder.RegisterType<EfUserDal>().As<IUserDal>();
+            builder.RegisterType<EfUserNewDal>().As<IUserNewDal>();
+            
+
+            builder.RegisterType<AuthManager>().As<IAuthService>();
+            builder.RegisterType<AuthNewManager>().As<IAuthNewService>();
+            builder.RegisterType<JwtHelper>().As<ITokenHelper>();
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                {
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
+        }
+    }
+}
