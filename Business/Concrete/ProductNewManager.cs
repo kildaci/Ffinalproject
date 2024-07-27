bb87b5ed033d4d97ee59c645ef6984ee94d539c4
Queen A <139057414+kildaci@users.noreply.github.com>
2024-07-27 23:43:18 +0300

    Create ProductNewManager.cs

---
+++ Business/Concrete/ProductNewManager.cs
@@ -0,0 +1,39 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProductNewManager : IProductNewService
+    {
+        IProductNewDal _productNewDal;
+
+        public ProductNewManager(IProductNewDal productNewDal)
+        {
+            _productNewDal = productNewDal;
+            
+        }
+        public IDataResult<List<ProductNew>> GetAll()
+        {
+            return new SuccessDataResult<List<ProductNew>>(_productNewDal.GetAll(), Messages.ProductListed);
+        }
+
+        public IDataResult<List<ProductNew>> GetByUnitPrice()
+        {
+            return new SuccessDataResult<List<ProductNew>>(_productNewDal.GetAll());
+        }
+
+        public IDataResult<List<ProductNewDetailDto>> GetProductDetailDtos()
+        {
+            return new SuccessDataResult<List<ProductNewDetailDto>>(_productNewDal.GetProductsDetails());
+        }
+    }
+}
