49c6fba51d82946457c46456f2bb024215fb7a83
Queen A <139057414+kildaci@users.noreply.github.com>
2024-07-27 23:42:39 +0300

    Create IProductNewService.cs

---
+++ Business/Abstract/IProductNewService.cs
@@ -0,0 +1,20 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IProductNewService
+    {
+        IDataResult<List<ProductNew>> GetAll();
+        IDataResult<List<ProductNew>> GetByUnitPrice();
+        IDataResult<List<ProductNewDetailDto>> GetProductDetailDtos();
+        //IDataResult<ProductNew> GetById(int productId);
+        
+    }
+}
