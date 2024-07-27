8d566c710a14b1beea354e9b508f372ef073598f
Queen A <139057414+kildaci@users.noreply.github.com>
2024-07-27 23:41:28 +0300

    Create IAuthNewService.cs
    new one

---
+++ Business/Abstract/IAuthNewService.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities.Concrete;
+using Core.Utilities.Results;
+using Core.Utilities.Security.JWT;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IAuthNewService
+    {
+        IDataResult<UserNew> Register(UserForRegisterDto userForRegisterDto, string password);
+        IDataResult<UserNew> Login(UserForLoginDto userForLoginDto);
+        IDataResult<AccessToken> CreateAccessToken(UserNew userNew);
+        IResult UserExists(string email);
+        IResult ResetPassword(string userName);
+        IDataResult<UserNew> Logout(UserForLoginDto userForLoginDto);
+
+    }
+}
