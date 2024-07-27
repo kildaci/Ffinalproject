f2a53f06f3f463a590e2f78ee315eafe3defade3
Queen A <139057414+kildaci@users.noreply.github.com>
2024-07-27 23:43:26 +0300

    Create UserNewManager.cs

---
+++ Business/Concrete/UserNewManager.cs
@@ -0,0 +1,43 @@
+﻿using Business.Abstract;
+using Core.Entities.Concrete;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class UserNewManager : IUserNewService
+    {
+        IUserNewDal _userNewDal;
+
+        public UserNewManager(IUserNewDal userNewDal)
+        {
+            _userNewDal = userNewDal;
+        }
+        public void Add(UserNew userNew)
+        {
+            _userNewDal.Add(userNew);
+
+        }
+
+        public IDataResult<List<UserNew>> GetAll()
+        {
+            return new SuccessDataResult<List<UserNew>>(_userNewDal.GetAll());
+         }
+
+        public UserNew GetByMail(string email)
+        {
+            return _userNewDal.Get(u => u.Email == email);
+        }
+
+        public List<OperationClaim> GetClaims(UserNew userNew)
+        {
+            return _userNewDal.GetClaims(userNew);
+
+        }
+    }
+}
